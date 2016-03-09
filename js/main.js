442d3e30660c13bd2e7b8fc753283d18a8f99b29
Gabriel Mojica <niko.mojica@live.com>
2016-03-10 03:24:34 +1100

    refactor: Convert tabs to spaces

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-var food = Backbone.Model.extend({
-	name: this.get('name')
+var app = app || {};
+
+app.Food = Backbone.Model.extend({
+    name: ''
 });
+
+app.FoodCollection = Backbone.Collection.extend({
+    model: app.Food
+});
+
+app.FoodCollectionView = Backbone.View.extend({
+    events: {
+        'click #search-button': 'addFood'
+    },
+    initialize: function() {
+        this.collection = new app.FoodCollection();
+        this.render();
+    },
+    render: function() {
+        console.log('fish fish');
+    },
+    addFood: function() {
+        // $('#foodCollection').append('<p>This is a food</p>');
+        console.log('miko');
+    }
+});
+
+var foodCollection = new app.FoodCollectionView();

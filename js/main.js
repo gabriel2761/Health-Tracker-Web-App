da98c2a00e4fed7a5db3f521bf01fda2c5922c87
Gabriel Mojica <niko.mojica@live.com>
2016-03-12 05:42:46 +1100

    test: Alert when button is pressed

--- js/main.js
+++ js/main.js
@@ -8,17 +8,21 @@ app.Food = Backbone.Model.extend({
 
 app.FoodView = Backbone.View.extend({
     el: '#search-container',
+    events: {
+        'click': 'alertUser'
+    },
     initialize: function() {
-        this.template = _.template('<button class="search-button">Search<%= name %></button>');
+        this.template = _.template('<button class="search-button">Search</button>');
         this.render();
     },
     render: function() {
         this.$el.html(this.template());
+    },
+    alertUser: function() {
+        alert("This works");
     }
 });
 
 var foodView = new app.FoodView({
     model: new app.Food()
 });
-
-console.log(foodView.toJSON());

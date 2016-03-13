54b8c0efdde41affbc7caadc89b213e0f769d6a7
Gabriel Mojica <niko.mojica@live.com>
2016-03-14 05:11:08 +1100

    feat: Add food collection view

--- js/main.js
+++ js/main.js
@@ -23,11 +23,41 @@ app.FoodSearchView = Backbone.View.extend({
     }
 });
 
-app.FoodItemView = Backbone.View.extend({
-    tagName: 'list',
+app.FoodCollectionView = Backbone.View.extend({
+    tagName: 'section',
+    render: function() {
+        this.collection.each(this.addFood, this);
+        return this;
+    },
+    addFood: function(food) {
+        var foodItemView = new app.FoodItemView({ model: food });
+        this.$el.append(foodItemView.render().el);
+    }
 });
 
-var pizza = new app.FoodModel();
+app.FoodItemView = Backbone.View.extend({
+    tagName: 'section',
+    className: 'food-item',
+    template: _.template($('#food-item').html()),
+    render: function() {
+        var foodTemplate = this.template(this.model.toJSON());
+        this.$el.html(foodTemplate);
+        return this;
+    },
+});
+
+app.FoodCollection = Backbone.Collection.extend({
+    model: app.FoodModel
+});
+
+var pizza = new app.FoodModel({ name: 'pizza' });
+
+var foodCollection = new app.FoodCollection([
+    pizza
+]);
+
+
+var foodCollectionView = new app.FoodCollectionView({ collection: foodCollection });
 
 var foodSearchView = new app.FoodSearchView({
     model: new app.FoodModel()

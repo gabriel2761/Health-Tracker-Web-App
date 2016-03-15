32f36e606b46ded196516139e0d8133cb23823d9
Gabriel Mojica <niko.mojica@live.com>
2016-03-16 05:28:10 +1100

    refactor: Remove search bar and redundant collections

--- js/main.js
+++ js/main.js
@@ -7,37 +7,8 @@ app.FoodModel = Backbone.Model.extend({
     }
 });
 
-app.FoodCollection = new Backbone.Collection.extend({
+app.FoodCollection = Backbone.Collection.extend({
     model: app.FoodModel
-});
-
-app.FoodSearchView = Backbone.View.extend({
-    el: '#search-container',
-    events: {
-        'click #search-button': 'appendFood'
-    },
-    initialize: function() {
-        this.template = _.template('<button id="search-button" class="search-button">Search</button>');
-        this.render();
-    },
-    render: function() {
-        this.$el.html(this.template());
-    },
-    appendFood: function() {
-        $('#food-list').append('<li>this works</li>');
-    }
-});
-
-app.FoodCollectionView = Backbone.View.extend({
-    tagName: 'section',
-    render: function() {
-        this.collection.each(this.addFood, this);
-        return this;
-    },
-    addFood: function(food) {
-        var foodItemView = new app.FoodItemView({ model: food });
-        this.$el.append(foodItemView.render().el);
-    }
 });
 
 app.FoodItemView = Backbone.View.extend({
@@ -51,10 +22,17 @@ app.FoodItemView = Backbone.View.extend({
     },
 });
 
-app.FoodCollection = Backbone.Collection.extend({
-    model: app.FoodModel
+app.FoodCollectionView = Backbone.View.extend({
+    tagName: 'section',
+    render: function() {
+        this.collection.each(this.addFood, this);
+        return this;
+    },
+    addFood: function(food) {
+        var foodItemView = new app.FoodItemView({ model: food });
+        this.$el.append(foodItemView.render().el);
+    }
 });
-
 
 var foodCollection = new app.FoodCollection([
     new app.FoodModel({ name: 'Pizza' }),

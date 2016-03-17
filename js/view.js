b2ea37ff92ad91b4c318eeaa3c029d2d6b16c8ac
Gabriel Mojica <niko.mojica@live.com>
2016-03-18 02:06:14 +1100

    refactor: Remove console log

--- js/view.js
+++ js/view.js
@@ -5,7 +5,6 @@ app.FoodItemView = Backbone.View.extend({
     className: 'food-item',
     template: _.template($('#food-item').html()),
     render: function() {
-        console.log($('#food-item').html());
         var foodTemplate = this.template(this.model.toJSON());
         this.$el.html(foodTemplate);
         return this;

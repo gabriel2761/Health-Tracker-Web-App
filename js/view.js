5ddce2ee9c8e04745e96194ed012c9c216be5b3c
Gabriel Mojica <niko.mojica@live.com>
2016-03-18 08:50:03 +1100

    fix: Only update list when searching

--- js/view.js
+++ js/view.js
@@ -19,13 +19,13 @@ app.FoodCollectionView = Backbone.View.extend({
     },
     initialize: function() {
         this.collection.on('add', this.addFood, this);
-        this.collection.on('reset', this.render, this);
     },
     render: function() {
         this.$el.html(this.template);
         return this;
     },
     search: function() {
+        $('#foodlist').empty();
         var food = $('#search-bar').val();
         this.collection.reset();
         this.collection.search(food);
@@ -33,9 +33,9 @@ app.FoodCollectionView = Backbone.View.extend({
     },
     addFood: function(food) {
         var foodItemView = new app.FoodItemView({ model: food });
-        this.$el.append(foodItemView.render().el);
+        $('#foodlist').append(foodItemView.render().el);
     },
     checkEnterPressed: function(event) {
-        if (event.keyCode === 13) this.search()
+        if (event.keyCode === 13) this.search();
     },
 });

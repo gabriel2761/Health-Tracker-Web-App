6af85cb332d2971404e2680e0d85201879200cdc
Gabriel Mojica <niko.mojica@live.com>
2016-03-18 08:52:44 +1100

    refactor: Remove dummy food items

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 var app = app || {};
 
-var foodCollection = new app.FoodCollection([
-    new app.FoodModel({ name: 'Pizza' }),
-    new app.FoodModel({ name: 'Carrot' }),
-    new app.FoodModel({ name: 'Quiche' }),
-    new app.FoodModel({ name: 'Apple' }),
-    new app.FoodModel({ name: 'Bannana' }),
-    new app.FoodModel({ name: 'Chicken' }),
-    new app.FoodModel({ name: 'Pad-see-ew' }),
-]);
+var foodCollection = new app.FoodCollection([]);
 
 
 var foodCollectionView = new app.FoodCollectionView({ collection: foodCollection });
 $('#container').html(foodCollectionView.render().el);
-
-
-foodCollection.add(new app.FoodModel({ name: 'fish' }));
-foodCollection.add(new app.FoodModel({ name: 'chips' }));
-foodCollection.add(new app.FoodModel({ name: 'meatballs' }));

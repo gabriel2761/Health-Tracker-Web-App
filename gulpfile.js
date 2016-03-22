7c3356cf8981e945c93ffbbc7ac6c861048d461c
Gabriel Mojica <niko.mojica@live.com>
2016-03-23 10:00:14 +1100

    test: Convert sass to css in a test.css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 gulp.task('default', function() {
-    console.log('test');
+
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/sass/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css/test.css'));
 });

4e7a32fdc5546ccccc98d1edd853998b03cb51a2
Gabriel Mojica <niko.mojica@live.com>
2016-03-25 07:03:31 +1100

    chore: Inline css with gulp-inline-source plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    inlinesource = require('gulp-inline-source');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('inlinesource', function() {
+    return gulp.src('src/index.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('sass', function() {

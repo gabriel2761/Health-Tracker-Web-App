ecb9cd59a592ea92b0edd80587139712eb95661c
Gabriel Mojica <niko.mojica@live.com>
2016-03-26 07:23:03 +1100

    refactor: Clean up main.js code layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,14 @@ var app = app || {};
 
 var foodCollection = new app.FoodCollection([]);
 var profileModel = new app.ProfileModel({});
-
 var navigationView = new app.NavigationView({});
+var foodCollectionView = new app.FoodCollectionView({ collection: foodCollection, profile: profileModel });
+var profileView = new app.ProfileView({ model: profileModel });
 
 $('#main-container').prepend(navigationView.render().el);
-
-var foodCollectionView = new app.FoodCollectionView({ collection: foodCollection, profile: profileModel });
 $('#foods').html(foodCollectionView.render().el);
-
-var profileView = new app.ProfileView({ model: profileModel });
 $('#profile').html(profileView.render().el);
+
 profileView.update();
 profileView.showRecent();
-
 

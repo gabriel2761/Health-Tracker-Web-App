afe71526084d43970f303beac855663caeebe1dc
Gabriel Mojica <niko.mojica@live.com>
2016-04-03 05:13:03 +1000

    docs: Create jsdoc for database.js

--- src/js/database.js
+++ src/js/database.js
@@ -1,5 +1,14 @@
 var app = app || {};
 
+/**
+ * Database that stores localStorage values.
+ * Creates two key constants for saving values,
+ * this.KEY for storing food-item information and
+ * this.ID_COUNT for storing a unique ID for each food-item.
+ * if localStorage returns null when retrieving the values,
+ * then the keys are set with empty information.
+ * @constructor
+ */
 app.Database = function() {
     this.KEY = 'awef83hriwalgbwaeg8ahwpfoiengp9seugbzspi3uhfsozigbps9zugbspigbp3PFQFNZKLNVZSKEFBSLEIGUBSPG9UQB';
     var items = JSON.parse(localStorage.getItem(this.KEY));
@@ -10,16 +19,28 @@ app.Database = function() {
     if (id === null) localStorage.setItem(this.ID_COUNT, 0);
 };
 
+/**
+ * Increments the ID value and returns a unique ID for a food-item
+ * @return {Integer} unique value for the database
+ */
 app.Database.prototype.getId = function() {
     var id = parseInt(localStorage.getItem(this.ID_COUNT));
     localStorage.setItem(this.ID_COUNT, ++id);
     return id;
 };
 
+/**
+ * Retrieves all food items from the localStorage
+ * @return {array} objects containing food item information
+ */
 app.Database.prototype.getFoods = function() {
     return JSON.parse(localStorage.getItem(this.KEY));
 };
 
+/**
+ * Adds a food item to localStorage
+ * @param {FoodModel} food Item to be stored
+ */
 app.Database.prototype.addFood = function(food) {
     var today = new Date(),
         day = today.getDate(),
@@ -38,9 +59,15 @@ app.Database.prototype.addFood = function(food) {
         calories: food.get('calories'),
         brandname: food.get('brandname')
     });
+
     localStorage.setItem(this.KEY, JSON.stringify(foods));
 };
 
+/**
+ * Removes a food item from localStorage, by finding
+ * the id of the selected food-item from localStorage.
+ * @param  {FoodModel} model FoodModel to be removed
+ */
 app.Database.prototype.removeFood = function(model) {
     var foods = this.getFoods();
 
